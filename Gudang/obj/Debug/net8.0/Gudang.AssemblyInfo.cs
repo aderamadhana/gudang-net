d6802de66fa5cbea6da1f4a3bb0332f105e810c5
aderamadhana <sanade2034@gmail.com>
2025-09-16 14:05:38 +0700

    update gitignore

--- Gudang/obj/Debug/net8.0/Gudang.AssemblyInfo.cs
+++ Gudang/obj/Debug/net8.0/Gudang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gudang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ccb6963c56c7934879c364dbfb3f41e031f922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4047b5c7371ecb205f6f2f8c9ac39efd12da09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gudang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gudang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
